c3ae0d96f68a983338297114023104f68ad6e8b1
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-19 12:35:18 +0900

    add js of the speech

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -150,5 +150,39 @@ newPostRef.ref(room).on("child_added", function(data) {
     output.innerHTML += str;
 });
 </script>
+<script>
+const speech = new webkitSpeechRecognition();
+speech.lang = 'ja-JP';
+
+const btn = document.getElementById('btn');
+const content = document.getElementById('content');
+
+btn.addEventListener('click' , function() {
+// 音声認識をスタート
+speech.start();
+});
+
+//---------------追記---------------//
+//音声自動文字起こし機能
+speech.onresult = function(e) {
+    speech.stop();
+    if(e.results[0].isFinal){
+        var autotext =  e.results[0][0].transcript
+        console.log(e);
+        console.log(autotext);
+        // content.innerHTML += '<div>'+ autotext +'</div>';
+        newPostRef.ref(room).push({
+            username: username.value,
+            text: autotext,
+            time: time()
+        });
+    }
+};
+
+speech.onend = () => { 
+    speech.start() 
+};
+
+</script>
 </body>
 </html>
