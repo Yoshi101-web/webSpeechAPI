32415c6db66a17a67583224172668d9ba64c2d7f
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-19 12:02:36 +0900

    add firebase

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -18,5 +18,30 @@
         </div>
         <div id="output"></div>
     </div>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-database.js"></script> 
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+    https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js"></script>
+
+<script>
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD8YCC2jpsLVEppVOpjjjOR93eRCX2gyLM",
+    authDomain: "webspeechapi-1760c.firebaseapp.com",
+    databaseURL: "https://webspeechapi-1760c.firebaseio.com",
+    projectId: "webspeechapi-1760c",
+    storageBucket: "webspeechapi-1760c.appspot.com",
+    messagingSenderId: "1038562880957",
+    appId: "1:1038562880957:web:d153b95c36d56e3847b6d4",
+    measurementId: "G-D8W8GJ7JCX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+</script>
 </body>
 </html>
